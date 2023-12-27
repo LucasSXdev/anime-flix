fbc324b7e3ba22468c8ddf6627e4806c98b8ddad
LUCAS DE SOUZA XAVIER <lucaswats39@gmail.com>
2023-12-27 12:09:41 -0300

    adicionando uma lista de personagens

--- ListaAnimes/src/pages/AnimeInfo/AnimeInfo.jsx
+++ ListaAnimes/src/pages/AnimeInfo/AnimeInfo.jsx
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import api from "../../services/Api";
 import { useParams } from "react-router-dom";
-import { Container, Description } from "./Styles";
+import {
+  AreaButtons,
+  Container,
+  ContainerCharacters,
+  Description,
+  StyledUl,
+} from "./Styles";
 
 export default function AnimeInfo() {
   const { mal_id } = useParams();
   const [animes, setAnimes] = useState({});
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     const fetchAnimes = async () => {
@@ -15,32 +22,61 @@ export default function AnimeInfo() {
       setAnimes(responseJson.data);
     };
 
+    const fetchCharacters = async () => {
+      const response = await fetch(`${api}/anime/${mal_id}/characters`);
+      const responseJson = await response.json();
+      console.log(responseJson.data.slice(0, 10));
+      setCharacters(responseJson.data);
+    };
+
     fetchAnimes();
+    fetchCharacters();
   }, [mal_id]);
 
   return (
-    <Container>
-      <Description>
-        <h1>{animes.title}</h1>
-        <strong>Sinopse:</strong>
-        <p>{animes.synopsis}</p>
-        <strong>Nota Média:{animes.score}</strong>
-        <strong>
-          Gêneros:
-          <ul>
-            {animes.genres.map((genre) => {
-              return (
-                <li key={genre.mal_id}>
-                  <p>{genre.name}</p>
-                </li>
-              );
+    <>
+      <Container>
+        <Description>
+          <h1>{animes.title}</h1>
+          <strong>Sinopse:</strong>
+          <p>{animes.synopsis}</p>
+          <strong>Nota Média:{animes.score}</strong>
+          <StyledUl>
+            {animes?.genres?.map((genre) => {
+              return <li key={genre.mal_id}>{genre.name}</li>;
             })}
-          </ul>
-        </strong>
-      </Description>
-      {animes.images && animes.images.jpg && (
-        <img src={animes.images.jpg.large_image_url} alt={animes.title} />
-      )}
-    </Container>
+          </StyledUl>
+          <AreaButtons>
+            <button>Salvar</button>
+            <a
+              target="blank"
+              href={`https://youtube.com/results?search_query=${animes.title} trailer}`}
+            >
+              <button>Trailer</button>
+            </a>
+          </AreaButtons>
+        </Description>
+        {animes.images && animes.images.jpg && (
+          <img src={animes.images.jpg.large_image_url} alt={animes.title} />
+        )}
+      </Container>
+      <ContainerCharacters>
+        <h2>Lista de personagens</h2>
+        <ul>
+          {characters.slice(0, 20).map((character) => {
+            return (
+              <li key={character.mal_id}>
+                <strong>{character.character.name}</strong>
+                <img
+                  src={character.character?.images?.jpg?.image_url}
+                  alt={character.character.name}
+                />
+                <p>{character.character.about}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </ContainerCharacters>
+    </>
   );
 }
--- ListaAnimes/src/pages/AnimeInfo/Styles.js
+++ ListaAnimes/src/pages/AnimeInfo/Styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  align-items: center;
   padding-top: 10px;
 
   img {
@@ -11,6 +12,12 @@ export const Container = styled.div`
     width: 40vw;
     height: 90vh;
     object-fit: cover;
+  }
+
+  Strong {
+    display: block;
+    margin-top: 15px;
+    margin-bottom: 15px;
   }
 `;
 
@@ -20,5 +27,66 @@ export const Description = styled.div`
 
   p {
     font-size: 16px;
+  }
+`;
+
+export const StyledUl = styled.div`
+  list-style: none;
+  display: flex;
+  gap: 15px;
+  width: 60%;
+
+  li {
+    padding: 5px;
+    color: var(--text-color-dark);
+    background: var(--background-dark);
+    font-weight: bold;
+  }
+`;
+
+export const AreaButtons = styled.div`
+  display: flex;
+  width: 80%;
+  margin-top: 15px;
+  gap: 10px;
+
+  button {
+    width: 200px;
+    padding: 10px;
+    border: 2px solid;
+    font-weight: bold;
+    cursor: pointer;
+    transition: 0.5s ease-in-out;
+
+    &:hover {
+      color: white;
+      background: var(--primary-color-dark);
+    }
+  }
+`;
+
+export const ContainerCharacters = styled.div`
+  width: 100vw;
+  margin: 10px;
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+    gap: 8px;
+    list-style: none;
+
+    li {
+      width: 150px;
+      margin-top: 10px;
+      height: 200px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
   }
 `;
